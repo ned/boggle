41e768b019d82c064aa80568a54aa1dfaba1cbf7
enchilado <www.e.sayers@gmail.com>
2019-02-28 17:35:30 +1000

    Add timer, adjust letter frequencies, Qu > Q
    Letter frequencies are cube rooted to bring them closer together, and vowels are square rooted an additional time to make them slightly more common. Letter frequencies are also squared each time they are picked to make multiple of the same letter less common.

--- index.js
+++ index.js
@@ -1,54 +1,96 @@
-window.onload = () => {
-	const root = document.getElementById("root");
+let time = 180;
+const timer = setInterval(tickTimer, 1000);
+var endgame = new Audio('endgame.mp3');
 
-	const weights = {
-		  'A': 8.167
-		, 'B': 1.492
-		, 'C': 2.782
-		, 'D': 4.253
-		, 'E': 12.702
-		, 'F': 2.228
-		, 'G': 2.015
-		, 'H': 6.094
-		, 'I': 6.966
-		, 'J': 0.153
-		, 'K': 0.772
-		, 'L': 4.025
-		, 'M': 2.406
-		, 'N': 6.749
-		, 'O': 7.507
-		, 'P': 1.929
-		, 'Qu': 0.095
-		, 'R': 5.987
-		, 'S': 6.327
-		, 'T': 9.056
-		, 'U': 2.758 - 0.095 // freq(u) - freq(q)
-		, 'V': 0.978
-		, 'W': 2.360
-		, 'X': 0.150
-		, 'Y': 1.974
-		, 'Z': 0.074
+const weights = {
+		  'A': 0.08167
+		, 'B': 0.01492
+		, 'C': 0.02782
+		, 'D': 0.04253
+		, 'E': 0.12702
+		, 'F': 0.02228
+		, 'G': 0.02015
+		, 'H': 0.06094
+		, 'I': 0.06966
+		, 'J': 0.00153
+		, 'K': 0.00772
+		, 'L': 0.04025
+		, 'M': 0.02406
+		, 'N': 0.06749
+		, 'O': 0.07507
+		, 'P': 0.01929
+		, 'Q': 0.00095
+		, 'R': 0.05987
+		, 'S': 0.06327
+		, 'T': 0.09056
+		, 'U': 0.02758 - 0.00095 // freq(u) - freq(q)
+		, 'V': 0.00978
+		, 'W': 0.02360
+		, 'X': 0.00150
+		, 'Y': 0.01974
+		, 'Z': 0.00074
 		};
-	
-	const distribution = makeDistribution(weights);
+
+window.onload = () => {
+	setTimer(time);
+	const root = document.getElementById("board");
+	const vowels = ["A", "E", "I", "O", "U"];
+
+
+	for (const [key, weight] of Object.entries(weights)) {
+		weights[key] = Math.cbrt(weights[key]);
+		if (vowels.includes(key)) {
+			weights[key] = Math.sqrt(weights[key]);
+		}
+	}
 	
 	const letters = [];
 	for (let i = 0; i < 16; i++) {
+		distribution = makeDistribution(weights);
 		letter = randomElement(distribution);
+		weights[letter] = Math.pow(weights[letter],2);
 		rotation = randomElement([0, 90, 180, 270]);
-		createLetter(root, letter, rotation);
+		createLetter(root, letter, rotation, i);
 	}
+
+	for (let i = 0; i < 16; i++) {
+		tile = document.getElementById(`letter ${i}`);
+		letter = tile.textContent;
+		if (letter == "Q") {
+			qcolumn = i % 4;
+			qrow = Math.floor(i / 4);
+			console.log(qcolumn);
+			console.log(qrow);
+			console.log(letter);
+			validtiles = [];
+			for (let column = 0; column < 4; column++) {
+				for (let row = 0; row < 4; row++) {
+					columndiff = Math.abs(column - qcolumn)
+					rowdiff = Math.abs(row - qrow)
+					if (columndiff <= 1 && rowdiff <= 1 && columndiff + rowdiff > 0) {
+						validid = row * 4 + column;
+						validtiles.push(validid);
+					}
+
+				}
+			}
+			uid = randomElement(validtiles);
+			utile = document.getElementById(`letter ${uid}`);
+			utile.textContent = "U";
+		}
+	}
+
 }
 
 function randomElement(list) {
-	const index = Math.floor(Math.random() * list.length);
+	const index = Math.floor(Math.random() * list.length);	
 	return list[index];
 }
 
 function makeDistribution(weights) {
 	const distribution = []
 	for (const [key, weight] of Object.entries(weights)) {
-		const resolution = 10;
+		const resolution = 10000;
 		const frequency = Math.floor(weight * resolution);
 		for (let i = 0; i < frequency; i++) {
 			distribution.push(key);
@@ -57,10 +99,11 @@ function makeDistribution(weights) {
 	return distribution;
 }
 
-function createLetter(root, letter, rotation) {
+function createLetter(root, letter, rotation, id) {
 	const node = document.createElement("div");
 	const textNode = document.createElement("span");
-
+	node.setAttribute("id",`tile ${id}`)
+	textNode.setAttribute("id",`letter ${id}`)
 	textNode.textContent = letter;
 	textNode.style.transform = `rotate(${rotation}deg)`;
 
@@ -71,4 +114,36 @@ function createLetter(root, letter, rotation) {
 
 	node.appendChild(textNode);
 	root.appendChild(node);
+}
+
+function pad(padding, n) {
+	let result = "";
+	for (let i = 0; i < padding; i++) {
+		result = n % 10 + result;
+		n = Math.floor(n / 10);
+	}
+	return result;
+}
+
+function setTimer(time) {
+	const timeNode = document.getElementById("timer");
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+	timeNode.textContent = `${minutes}:${pad(2, seconds)}`;
+}
+
+function tickTimer() {
+	time = time - 1;
+	setTimer(time);
+	if (time <= 0) {
+		for (let i = 0; i < 16; i++) {
+			const tileNode = document.getElementById(`tile ${i}`);
+			tileNode.classList.add('greytile');
+		}
+		const timeNode = document.getElementById("timer");
+		clearInterval(timer);
+		timeNode.textContent = `End!`;
+		endgame.play();
+	}
+	return;
 }
